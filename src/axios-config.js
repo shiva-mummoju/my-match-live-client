3069d8337e73b5436d5346b15ec6e4207c1154bc
shiva <shiva.sairam97@gmail.com>
2018-08-20 01:06:56 +0530

    deployment bug

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/'
+    // baseURL: 'http://localhost:5000/'
+    baseURL: "https://rocky-brook-58200.herokuapp.com/"
 });
 
 export default instance;
