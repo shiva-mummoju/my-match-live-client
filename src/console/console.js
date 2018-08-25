cf3c11475beb19d9ee8cd121223e23da9dfd94f1
shiva <shiva.sairam97@gmail.com>
2018-08-25 12:48:55 +0530

    run button styling to make mutually exclusive

--- src/console/console.js
+++ src/console/console.js
@@ -24,13 +24,15 @@ const customStyles = {
 Modal.setAppElement("#root");
 
 class Console extends Component {
+  
   constructor(props) {
     super(props);
 
     this.state = {
       modalIsOpen: false,
       copyMessage: "click to copy",
-      matchLink: "Currently Not available"
+      matchLink: "Currently Not available",
+      button :  [0,0,0,0,0,0,0,0]
     };
 
     this.openModal = this.openModal.bind(this);
@@ -111,6 +113,24 @@ class Console extends Component {
     this.setState({ copyMessage: "message has been copied" });
   };
 
+  getButtonClasses(num){
+    var cla = "";
+    if(!this.state.button[num]){
+      cla = "col-lg-1 col-sm-1  col-xs-1 option";
+    }else{
+      cla = "col-lg-1 col-sm-1  col-xs-1 option blue";
+    }
+    return cla;
+  }
+
+  runButtonClicked(num){
+    var button  = [0,0,0,0,0,0,0,0];
+    button[num] = !this.state.button[num];
+    this.setState({button: button});
+  }
+
+  
+
   render() {
     return (
       <div>
@@ -138,10 +158,10 @@ class Console extends Component {
         </Modal>
         <div className="main">
           <div className="row ">
-            <div className="col-lg-3 col-sm-3" />
-            <div className="col-lg-6 col-sm-6 scorecard">
+            <div className="col-lg-3 col-sm-3 col-xs-1" />
+            <div className="col-lg-6 col-sm-6 col-xs-10 scorecard">
               <div className="row mainscore ">
-                <div className="col-lg-12 col-sm-12">
+                <div className="col-lg-12 col-sm-12  col-xs-12">
                   <div className="row mainscore-score ">
                     ind 413 / 6 <span> (87.5 overs) crr: 3.53 </span>
                     <button
@@ -159,72 +179,76 @@ class Console extends Component {
                 {/* <div className="col-lg-6 col-sm-6" /> */}
               </div>
               <div className="row batsman ">
-                <div className="col-lg-6 col-sm-6 ">BATSMAN</div>
-                <div className="col-lg-1 col-sm-1">Runs</div>
-                <div className="col-lg-1 col-sm-1">Balls</div>
-                <div className="col-lg-1 col-sm-1">fours</div>
-                <div className="col-lg-1 col-sm-1">six</div>
-                <div className="col-lg-2 col-sm-2">sr</div>
+                <div className="col-lg-6 col-sm-6   col-xs-6">BATSMAN</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">Runs</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">Balls</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">fours</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">six</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2">sr</div>
               </div>
               <div className="row striker">
-                <div className="col-lg-6 col-sm-6">rishab</div>
-                <div className="col-lg-1 col-sm-1">12</div>
-                <div className="col-lg-1 col-sm-1">34</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">6</div>
-                <div className="col-lg-2 col-sm-2">sr</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6">rishab</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">12</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">34</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">23</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">6</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2">sr</div>
               </div>
               <div className="row striker ">
-                <div className="col-lg-6 col-sm-6">hardik</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">34</div>
-                <div className="col-lg-1 col-sm-1">6</div>
-                <div className="col-lg-2 col-sm-2">45</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6">hardik</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">23</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">23</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">34</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">6</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2">45</div>
               </div>
               <div className="row bowler">
-                <div className="col-lg-6 col-sm-6">BOWLER</div>
-                <div className="col-lg-1 col-sm-1">O</div>
-                <div className="col-lg-1 col-sm-1">M</div>
-                <div className="col-lg-1 col-sm-1">R</div>
-                <div className="col-lg-1 col-sm-1">W</div>
-                <div className="col-lg-2 col-sm-2">Eco</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6">BOWLER</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">O</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">M</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">R</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">W</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2">Eco</div>
               </div>
               <div className="row bowlerstats ">
-                <div className="col-lg-6 col-sm-6">Stokes</div>
-                <div className="col-lg-1 col-sm-1">4</div>
-                <div className="col-lg-1 col-sm-1">3</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-2 col-sm-2">12</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6">Stokes</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">4</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">3</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">23</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1">23</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2">12</div>
               </div>
 
               <div className="row info">OPTIONS</div>
 
               <div className="row  options">
-                <div className="col-lg-1 col-sm-1 option">0</div>
-                <div className="col-lg-1 col-sm-1 option">1</div>
-                <div className="col-lg-1 col-sm-1 option">2</div>
-                <div className="col-lg-1 col-sm-1 option">3</div>
-                <div className="col-lg-1 col-sm-1 option">4</div>
-                <div className="col-lg-1 col-sm-1 option">5</div>
-                {/* </div> */}
-                {/* <div className="row options"> */}
-                <div className="col-lg-1 col-sm-1 option">6</div>
-                <div className="col-lg-1 col-sm-1 option">7</div>
-                <div className="col-lg-1 col-sm-1 option">Wd</div>
-                <div className="col-lg-1 col-sm-1 option">Nb</div>
-                <div className="col-lg-1 col-sm-1 option">Lb</div>
-                <div className="col-lg-1 col-sm-1 option">W</div>
-                {/* </div> */}
-                {/* <div className="row options"> */}
-                <div className="col-lg-1 col-sm-1 option">RO</div>
-                <div className="col-lg-1 col-sm-1 option">by</div>
-                <div className="col-lg-1 col-sm-1 option">U</div>
+                <div className={this.getButtonClasses(0)}
+                 onClick={() => {this.runButtonClicked(0)}} >0</div>
+                <div className={this.getButtonClasses(1)} 
+                 onClick={() => {this.runButtonClicked(1)}} >1</div>
+                <div className={this.getButtonClasses(2)}
+                 onClick={() => {this.runButtonClicked(2)}} >2</div>
+                <div className={this.getButtonClasses(3)}
+                 onClick={() => {this.runButtonClicked(3)}} >3</div>
+                <div className={this.getButtonClasses(4)}
+                 onClick={() => {this.runButtonClicked(4)}} >4</div>
+                <div className={this.getButtonClasses(5)}
+                 onClick={() => {this.runButtonClicked(5)}} >5</div>
+                <div className={this.getButtonClasses(6)}
+                 onClick={() => {this.runButtonClicked(6)}} >6</div>
+                <div className={this.getButtonClasses(7)}
+                 onClick={() => {this.runButtonClicked(7)}} >7</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">Wd</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">Nb</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">Lb</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">W</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">RO</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">by</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 option">U</div>
               </div>
 
               <div className="row msgbox ">
-                <div className="col-lg-10 col-sm-10 msgbox-text  ">
+                <div className="col-lg-10 col-sm-10  col-xs-10 msgbox-text  ">
                   <textarea
                     type="text"
                     id="desc"
@@ -233,34 +257,34 @@ class Console extends Component {
                   />
                 </div>
                 <div
-                  className="col-lg-2 col-sm-2 sendButton"
+                  className="col-lg-2 col-sm-2  col-xs-2 sendButton"
                   onClick={this.ballBowled}
                 >
-                  Send{" "}
+                  Send
                 </div>
               </div>
             </div>
 
-            <div className="col-lg-3 col-sm-3" />
+            <div className="col-lg-3 col-sm-3 col-xs-1" />
           </div>
           <div className="row break">
-            <div className="col-lg-3 col-sm-3" />
-            <div className="col-lg-6 col-sm-6 scorecard">
-              <div className="col-lg-12  ">
+            <div className="col-lg-3 col-sm-3  col-xs-  col-xs-1" />
+            <div className="col-lg-6 col-sm-6   col-xs-10 scorecard">
+              <div className="col-lg-12   col-xs-12 ">
                 <div className="row oneballdesc ">Live Updates</div>
 
                 {this.state.match &&
                   this.state.match.balls.map((ball, id) => {
                     return (
                       <div className="row oneball " key={id}>
-                        <div className="col-lg-1 over">{ball.ballNumber}</div>
-                        <div className="col-lg-11">{ball.ballDesc}</div>
+                        <div className="col-lg-1   col-xs-1 over">{ball.ballNumber}</div>
+                        <div className="col-lg-11   col-xs-11">{ball.ballDesc}</div>
                       </div>
                     );
                   })}
               </div>
             </div>
-            <div className="col-lg-3 col-sm-3" />
+            <div className="col-lg-3 col-sm-3  col-xs-3" />
           </div>
         </div>
       </div>
