93c5ca2d40e472bf0633e19590b39b04d3667a50
shiva <shiva.sairam97@gmail.com>
2018-08-27 00:43:43 +0530

    html changes to score

--- src/score/score.js
+++ src/score/score.js
@@ -56,97 +56,115 @@ class Score extends Component {
   render() {
     return (
       <div>
+
         <Header />
+        {/* modal for sharing with friends */}
+        
+        {/* modal for choosing new bowler */}
+       
+        
         <div className="main">
           <div className="row ">
-            <div className="col-lg-3 col-sm-3" />
-            <div className="col-lg-6 col-sm-6 scorecard">
+            <div className="col-lg-3 col-sm-2 col-xs-1" />
+            <div className="col-lg-6 col-sm-8 col-xs-10 scorecard">
               <div className="row mainscore ">
-                <div className="col-lg-12 col-sm-12">
+                <div className="col-lg-12 col-sm-12  col-xs-12 nopadding">
                   <div className="row mainscore-score ">
-                    ind 413 / 6 <span> (87.5 overs) crr: 3.53 </span>
+                    {
+                     this.state.match ? this.state.match.team[this.state.match.currentBatting].name : "NA"
+                    }  &nbsp;
+                    {
+                      this.state.match ? this.state.match.team[this.state.match.currentBatting].score : "NA"
+                    }/ 
+                    {
+                      this.state.match ? this.state.match.team[this.state.match.currentBatting].wickets : "NA"
+                    }   <span> ({
+                      this.state.match ? Math.floor(this.state.match.team[this.state.match.currentBatting].ballsPlayed /6 ): "NA"
+                    }.{
+                      this.state.match ? this.state.match.team[this.state.match.currentBatting].ballsPlayed %6 : "NA"
+                    }) <br className="visible-xs"/>Rr: {this.state.match ? ((this.state.match.team[this.state.match.currentBatting].score /this.state.match.team[this.state.match.currentBatting].ballsPlayed)*6).toFixed(2) : '' } </span>
+                    
                     <button
                       className="btn btn-info"
                       onClick={this.onStatsClicked}
                     >
                       Stats
                     </button>
-                    <button
-                      className="btn btn-info"
-                      onClick={this.onStatsClicked}
-                    >
-                      Share
-                    </button>
                   </div>
-                  <div className="row innings">ind chose to bat</div>
+                  <div className="row innings">{this.state.match? this.state.match.status : ""}</div>
                 </div>
-                {/* <div className="col-lg-6 col-sm-6" /> */}
               </div>
               <div className="row batsman ">
-                <div className="col-lg-6 col-sm-6 ">BATSMAN</div>
-                <div className="col-lg-1 col-sm-1">Runs</div>
-                <div className="col-lg-1 col-sm-1">Balls</div>
-                <div className="col-lg-1 col-sm-1">fours</div>
-                <div className="col-lg-1 col-sm-1">six</div>
-                <div className="col-lg-2 col-sm-2">sr</div>
+                <div className="col-lg-6 col-sm-6   col-xs-6 nopadding">BATSMAN &nbsp; 
+
+                 </div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 nopadding">R</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 nopadding">B</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 nopadding">4</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 nopadding">6</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2 nopadding">sr</div>
               </div>
-              <div className="row striker">
-                <div className="col-lg-6 col-sm-6">rishab</div>
-                <div className="col-lg-1 col-sm-1">12</div>
-                <div className="col-lg-1 col-sm-1">34</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">6</div>
-                <div className="col-lg-2 col-sm-2">sr</div>
+              <div className="row striker onlyTopPadding">
+                <div className="col-lg-6 col-sm-6  col-xs-6 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].playerName: ""}* &nbsp; 
+
+                </div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].runsScored: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].ballsFaced: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].fours: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].sixes: ""}</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2 onlyTopPadding">{this.state.match? (this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].runsScored / this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentStriker].ballsFaced * 100).toFixed(2) : "" }</div>
               </div>
-              <div className="row striker ">
-                <div className="col-lg-6 col-sm-6">hardik</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">34</div>
-                <div className="col-lg-1 col-sm-1">6</div>
-                <div className="col-lg-2 col-sm-2">45</div>
+              <div className="row striker onlyTopPadding">
+                <div className="col-lg-6 col-sm-6  col-xs-6 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].playerName: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].runsScored: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].ballsFaced: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].fours: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].sixes: ""}</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2 onlyTopPadding">{this.state.match? (this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].runsScored / this.state.match.team[this.state.match.currentBatting].players[this.state.match.team[this.state.match.currentBatting].currentNonStriker].ballsFaced * 100).toFixed(2) : "" }</div>
               </div>
               <div className="row bowler">
-                <div className="col-lg-6 col-sm-6">BOWLER</div>
-                <div className="col-lg-1 col-sm-1">O</div>
-                <div className="col-lg-1 col-sm-1">M</div>
-                <div className="col-lg-1 col-sm-1">R</div>
-                <div className="col-lg-1 col-sm-1">W</div>
-                <div className="col-lg-2 col-sm-2">Eco</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6 onlyTopPadding">BOWLER &nbsp;
+                </div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">O</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">M</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">R</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">W</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2 onlyTopPadding">Eco</div>
               </div>
               <div className="row bowlerstats ">
-                <div className="col-lg-6 col-sm-6">Stokes</div>
-                <div className="col-lg-1 col-sm-1">4</div>
-                <div className="col-lg-1 col-sm-1">3</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-1 col-sm-1">23</div>
-                <div className="col-lg-2 col-sm-2">12</div>
+                <div className="col-lg-6 col-sm-6  col-xs-6 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].playerName: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? Math.floor(this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].ballsBowled / 6): ""}.{this.state.match?this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].ballsBowled % 6: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].maidens: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].runsGiven: ""}</div>
+                <div className="col-lg-1 col-sm-1  col-xs-1 onlyTopPadding">{this.state.match? this.state.match.team[this.state.match.currentBowling].players[this.state.match.team[this.state.match.currentBowling].currentBowler].wickets: ""}</div>
+                <div className="col-lg-2 col-sm-2  col-xs-2 onlyTopPadding">0</div>
               </div>
+              
             </div>
 
-            <div className="col-lg-3 col-sm-3" />
+            <div className="col-lg-3 col-sm-2 col-xs-1" />
           </div>
-
           <div className="row break">
-            <div className="col-lg-3 col-sm-3" />
-            <div className="col-lg-6 col-sm-6 scorecard">
-              <div className="col-lg-12  ">
+            <div className="col-lg-3 col-sm-2  col-xs-1" />
+            <div className="col-lg-6 col-sm-8   col-xs-10 scorecard ">
+              <div className="col-lg-12   col-xs-12 nopadding ">
                 <div className="row oneballdesc ">Live Updates</div>
 
-                {this.state &&
+                {this.state.match &&
                   this.state.match.balls.map((ball, id) => {
                     return (
                       <div className="row oneball " key={id}>
-                        <div className="col-lg-1 over">{ball.ballNumber}</div>
-                        <div className="col-lg-11">{ball.ballDesc}</div>
+                        <div className="col-lg-1   col-xs-1  over">{ball.ballNumber}</div>
+                        <div className="col-lg-11   col-xs-11">{ball.ballDesc}</div>
                       </div>
                     );
                   })}
               </div>
             </div>
-            <div className="col-lg-3 col-sm-3" />
+            <div className="col-lg-3 col-sm-2  col-xs-1" />
           </div>
         </div>
+        <ToastContainer />
       </div>
     );
   }
